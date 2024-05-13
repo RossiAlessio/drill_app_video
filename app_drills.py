e774dd0c04b812419db0193b539e57141afa3197
RossiAlessio <alessio.rossi2@gmail.com>
2024-05-13 15:23:13 +0200

    update credential

--- app_drills.py
+++ app_drills.py
@@ -4,12 +4,18 @@ from glob import glob
 from io import BytesIO
 from link_drive import upload_file_link
 from concurrent.futures import ThreadPoolExecutor
+from stqdm import stqdm
 import json
 import requests
-st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
+st.set_page_config(layout="wide",initial_sidebar_state='collapsed')ardcoded user credentials
+        
 
-# Define hardcoded user credentials
-USER_CREDENTIALS = {"marcello": "drill2024", "alessio": "admin2024"}
+# Frontend
+if 'title' not in st.session_state:
+    title = '<h1 style="font-size: 42px;">Drills library</h1>'
+    st.session_state['title'] = title
+
+st.markdown(st.session_state['title'], unsafe_allow_html=True)
 
 # Function to load image from Drive without resizing
 def load_image(url):
@@ -28,102 +34,77 @@ def load_images_async(urls):
             images.append(future.result())
     return images
 
-# Define the authenticate function
-def authenticate(username, password):
-    if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
-
-        # Frontend
-        if 'title' not in st.session_state:
-            title = '<h1 style="font-size: 42px;">Drills library</h1>'
-            st.session_state['title'] = title
-
-        st.markdown(st.session_state['title'], unsafe_allow_html=True)
-
-        # upload links file
-        with st.spinner('Update files...'):
-            dct_files = json.load(open('./drive_links.json'))
-            if 'fold_names' not in st.session_state:
-                folds = dct_files.keys()
-                st.session_state['fold_names'] = folds
-            
-                for fold in folds:
-                    st.session_state[fold] = {}
-                    urls = []
-                    for drill in dct_files[fold]:
-                        try:
-                            img_id = dct_files[fold][drill]['image']
-                            url_image = f"https://drive.google.com/uc?export=view&id={img_id}"
-                            urls.append(url_image)
-                            st.session_state[fold][drill] = {}
-                            st.session_state[fold][drill]['url_image'] = url_image
-                        except:
-                            pass
-                        
-                        try:
-                            video_id = dct_files[fold][drill]['video']
-                            url_video = f"https://drive.google.com/file/d/{video_id}/view?usp=drive_link"
-                            st.session_state[fold][drill]['url_video'] = url_video
-                        except:
-                            pass
-                    
-                    # Load images asynchronously
-                    images = load_images_async(urls)
-                    for drill, image in zip(dct_files[fold], images):
-                        st.session_state[fold][drill]['loaded_image'] = image
-                            
-        st.success('Update files...Done!')
-        
-        # Display the drill library
-        option = st.selectbox('Seleziona una categoria',[' '] + list(st.session_state['fold_names']))
-        if option != ' ':
-            try:
-                lst_drill = list(st.session_state[option].keys())
-                
-                c1,c2,c3,c4,c5 = st.columns(5)
-                n_rows = int(len(st.session_state[option])/5)+1
-                cols_order = [c1,c2,c3,c4,c5]*n_rows
-                
-                lst_zip = []
-                startN = 0
-                for row in range(n_rows):
-                    c1,c2,c3,c4,c5 = st.columns(5)
-                    values = dict( ((key, st.session_state[option][key]) for key in lst_drill[startN:startN+5]) )
-                    lst_zip.append([values,[c1,c2,c3,c4,c5]])
-                    startN += 5
-            
-                for vals,cols in lst_zip:
-                    with st.container():
-                        for i,col in zip(vals,cols): 
-                            with col:
-                                st.divider()
-                                st.markdown(i)
-                                try:
-                                    st.image(vals[i]['loaded_image'], use_column_width=True)
-                                except:
-                                    st.write('NO IMAGE')
-                                try:
-                                    st.link_button("Go to video", vals[i]['url_video'])
-                                except:
-                                    st.write('NO VIDEO')
-            except:
-                pass
-
-    else:
-        st.error("Invalid username or password")
-
-
-def main():
-    col1,col2,col3 = st.columns(3)
-    # Display login form
-    with col1:
-        username = st.text_input("Username")
-    with col2:
-        password = st.text_input("Password", type="password")
-    with col3:
-        login_button = st.button("Login")
+# upload links file
+with st.spinner('Update files...'):
+    dct_files = json.load(open('drive_links.json'))
+    if 'fold_names' not in st.session_state:
+        folds = dct_files.keys()
+        st.session_state['fold_names'] = folds
     
-    if login_button:
-        authenticate(username, password)
+        for fold in folds:
+            st.session_state[fold] = {}
+            urls = []
+            for drill in dct_files[fold]:
+                try:
+                    img_id = dct_files[fold][drill]['image']
+                    url_image = f"https://drive.google.com/uc?export=view&id={img_id}"
+                    urls.append(url_image)
+                    st.session_state[fold][drill] = {}
+                    st.session_state[fold][drill]['url_image'] = url_image
+                except:
+                    pass
+                
+                try:
+                    video_id = dct_files[fold][drill]['video']
+                    url_video = f"https://drive.google.com/file/d/{video_id}/view?usp=drive_link"
+                    st.session_state[fold][drill]['url_video'] = url_video
+                except:
+                    pass
+            
+            # Load images asynchronously
+            images = load_images_async(urls)
+            for drill, image in zip(dct_files[fold], images):
+                st.session_state[fold][drill]['loaded_image'] = image
+                    
+st.success('Update files...Done!')
 
-if __name__ == "__main__":
-    main()
+
+# # settings sidebar
+# with st.sidebar:
+#     st.markdown('SETTING')
+#     if st.button('Upload google drive links'):
+#         upload_file_link()
+
+
+
+option = st.selectbox('Seleziona una categoria',[' '] + list(st.session_state['fold_names']))
+if option != ' ':
+    st.write(option)
+    lst_drill = list(st.session_state[option].keys())
+    
+    c1,c2,c3,c4,c5 = st.columns(5)
+    n_rows = int(len(st.session_state[option])/5)+1
+    cols_order = [c1,c2,c3,c4,c5]*n_rows
+    
+    lst_zip = []
+    startN = 0
+    for row in range(n_rows):
+        c1,c2,c3,c4,c5 = st.columns(5)
+        values = dict( ((key, st.session_state[option][key]) for key in lst_drill[startN:startN+5]) )
+        lst_zip.append([values,[c1,c2,c3,c4,c5]])
+        startN += 5
+
+    for vals,cols in lst_zip:
+        with st.container():
+            for i,col in zip(vals,cols): 
+                with col:
+                    st.divider()
+                    st.markdown(i)
+                    try:
+                        st.image(vals[i]['loaded_image'], use_column_width=True)
+                    except:
+                        st.write('NO IMAGE')
+                    try:
+                        st.link_button("Go to video", vals[i]['url_video'])
+                    except:
+                        st.write('NO VIDEO')
